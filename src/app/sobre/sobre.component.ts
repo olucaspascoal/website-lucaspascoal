5a3abcc1c210467da4c3b0cc788ff77747298c91
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-13 23:39:00 -0300

    Inicio de ajustes na seção sobre

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
+  todayMonth: any;
+  semestreAtual: any;
+  today = new Date();
+  mes = this.today.getMonth() + 1;
+  ano = this.today.getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.mes < 10) {
+      this.todayMonth = '0' + this.mes;
+    } else {
+      this.todayMonth = this.mes.toString();
+    }
+    this.semestre()
   }
-
+  semestre(){
+    this.semestreAtual = (((this.ano + 1) - 2020) * 2);
+    if(this.todayMonth <= 6){
+      this.semestreAtual = this.semestreAtual - 1;
+    }
+  }
 }
